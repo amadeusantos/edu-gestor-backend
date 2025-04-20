fe7a0fd8d6c4f1eb3ef112ae985f86ce8e4e01fa
jose <josebazjunior09@gmail.com>
2025-04-20 10:32:49 -0300

    [refac]: move infra module inside core module and add database management logic

---
+++ core/infrastructure/database/session.py
@@ -0,0 +1,54 @@
+from typing import Callable, Optional, TypeVar
+from sqlalchemy.orm import scoped_session
+from core.infrastructure.database.engine import DatabaseEngine
+
+
+class LazySessionHolder:
+    def __init__(self, engine: DatabaseEngine) -> None:
+        self.session: Optional[scoped_session] = engine.get_session()
+
+    def commit(self) -> None:
+        if not self.session:
+            raise Exception("Session is not initialized.")
+        self.session.commit()
+
+    def rollback(self) -> None:
+        if not self.session:
+            raise Exception("Session is not initialized.")
+        self.session.rollback()
+
+    def close(self) -> None:
+        if self.session:
+            self.session.close()
+            self.session = None
+
+
+T = TypeVar("T")
+
+
+def db_session(func: Callable[..., T]) -> Callable[..., T]:
+    def wrapper(*args, **kwargs):
+        session_holder = LazySessionHolder()
+        try:
+            response = func(*args, **kwargs, session_holder=session_holder)
+            return response
+        except Exception as e:
+            session_holder.rollback()
+            raise e
+        finally:
+            session_holder.close()
+
+    return wrapper
+
+
+def get_session(**kwargs) -> scoped_session:
+    if "session_holder" not in kwargs:
+        raise KeyError("session holder parameter is required")
+
+    if type(kwargs["session_holder"]) != LazySessionHolder:
+        raise TypeError("session holder parameter must be of type LazySessionHolder")
+
+    if not kwargs["session_holder"].session:
+        raise ValueError("session holder parameter must be initialized")
+
+    return kwargs["session_holder"].session
