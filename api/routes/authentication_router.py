a3069ae761d6a036fac8b9b490b64ade47ed474b
jose <josebazjunior09@gmail.com>
2025-04-26 20:33:10 -0300

    [feat]: implement get_authenticated_user endpoint and update response models

--- api/routes/authentication_router.py
+++ api/routes/authentication_router.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+from fastapi import APIRouter, Depends, status
 from contracts.requests.auth_request import AuthenticationRequest
-from contracts.responses.auth_response import AuthenticationResponse
+from contracts.responses.auth_response import (
+    AuthenticationResponse,
+    CurrentUserResponse,
+)
 from contracts.responses.base import ProblemResponse
 from core.application.users.auth.login import login as login_service
 from core.infrastructure.database.manage import DbSession
+from core.infrastructure.database.tables import User
+from core.infrastructure.security.authorizer import get_current_user
 
 router = APIRouter()
 
@@ -20,3 +26,18 @@ def login(
     login_request: AuthenticationRequest, db_session: DbSession
 ) -> AuthenticationResponse:
     return login_service(login_request, db_session)
+
+
+@router.get(
+    "/me",
+    status_code=status.HTTP_200_OK,
+    response_model=CurrentUserResponse,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": ProblemResponse},
+    },
+)
+def get_authenticated_user(
+    _: DbSession,
+    current_user: Annotated[User, Depends(get_current_user)],
+) -> CurrentUserResponse:
+    return CurrentUserResponse.from_row(current_user)
--- contracts/responses/auth_response.py
+++ contracts/responses/auth_response.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from core.infrastructure.database.tables import User
+from typing import Optional
 
 
 class AuthenticationResponse(BaseModel):
@@ -15,4 +16,16 @@ class AuthenticationResponse(BaseModel):
             user_id=str(row.id),
             access_token=access_token,
             exp_time=exp_time,
+        )
+
+
+class CurrentUserResponse(BaseModel):
+    user_id: str
+    role: Optional[str]
+
+    @classmethod
+    def from_row(cls, row: User) -> "CurrentUserResponse":
+        return cls(
+            user_id=str(row.id),
+            role=row.profile.role.value if row.profile.role else None,
         )
